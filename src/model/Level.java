0dd2916950881e6880f12fba0c57b92c2ce08336
Thomas Copt-Bignon <lowx.add@gmail.com>
2021-11-20 15:16:26 +0100

    DrawPanel background finished

--- src/model/Level.java
+++ src/model/Level.java
@@ -1,6 +1,7 @@
 package model;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -10,9 +11,10 @@ import java.util.Scanner;
 public class Level {
 
 	private String currentState[][];
-	private int ressources[];
+	private int ressources[]; // C0, O0, L0, L1, T1, T2, T0, T3, F0, F1, F3, F2
 
 	public Level(int id) {
+		// Contruction de currentState en lisant le fichier de niveau
 		try {
 			Scanner s = new Scanner(new File("src/data/level" + id + ".p"));
 			int height = s.nextInt();
@@ -28,14 +30,49 @@ public class Level {
 			e.printStackTrace();
 		}
 
+		// Remplissage de ressources
 		ressources = new int[12];
+		Arrays.fill(ressources, 0);
 
+		for (int i = 1; i < currentState.length - 1; i++) {
+			for (int j = 1; j < currentState[0].length - 1; j++) {
+				String tile = currentState[i][j];
+				if (!tile.equals(".")) {
+					if (tile.charAt(0) != '*') {
+						switch (tile) {
+							case "C0" : ressources[0]++; break;
+							case "O0" : ressources[1]++; break;
+							case "L0" : ressources[2]++; break;
+							case "L1" : ressources[3]++; break;
+							case "T1" : ressources[4]++; break;
+							case "T2" : ressources[5]++; break;
+							case "T0" : ressources[6]++; break;
+							case "T3" : ressources[7]++; break;
+							case "F0" : ressources[8]++; break;
+							case "F1" : ressources[9]++; break;
+							case "F3" : ressources[10]++; break;
+							case "F2" : ressources[11]++; break;
+						}
+						currentState[i][j] = ".";
+					}
+				}
+			}
+		}
 	}
+
 	public int column() {
 		return currentState.length - 2;
 	}
 
 	public int line() {
 		return currentState[0].length - 2;
+	}
+
+	public int[] getRessources() {
+		return ressources;
+	}
+
+	public String[][] getCurrentState() {
+		return currentState;
 	}
 }
--- src/view/DrawPanelLevel.java
+++ src/view/DrawPanelLevel.java
@@ -11,19 +11,19 @@ public class DrawPanelLevel extends JPanel {
 	private Level l;
 	private BufferedImage background;
 
-	public DrawPanelLevel(Level l) {
-		int w = (l.column() + 4) * 120;
-		int h = Math.max(l.line() + 2, 6) * 120;
+	public DrawPanelLevel(Level level) {
+		int w = (level.column() + 4) * 120;
+		int h = Math.max(level.line() + 2, 6) * 120;
 
 		this.setPreferredSize(new Dimension(w, h));
-		this.l = l;
+		this.l = level;
 
 		// Création de l'image de fond
 		this.background = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 		Graphics g = background.createGraphics();
 
-		int xpipes = w - 240;
-		int ypipes = 0;
+		int x = w - 240;
+		int y = 0;
 
 		BufferedImage tile = Texture.getTextureTile(Special.DARKBROWN);
 		g.setColor(Color.black);
@@ -32,78 +32,144 @@ public class DrawPanelLevel extends JPanel {
 		// Dessin du fond la barre à tuyaux
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 6; j++) {
-				g.drawImage(tile, xpipes, ypipes, null);
-				ypipes += 120;
+				g.drawImage(tile, x, y, null);
+				y += 120;
 			}
-			xpipes += 120;
-			ypipes = 0;
+			x += 120;
+			y = 0;
 		}
 
 		// Dessin des coins
-		xpipes = 0;
-		ypipes = 0;
+		x = 0;
+		y = 0;
 		tile = Texture.getTextureTile(Special.CORNER);
-		g.drawImage(tile, xpipes, ypipes, null);
+		g.drawImage(tile, x, y, null);
 
-		xpipes += (l.column() + 1) * 120;
+		x += (level.column() + 1) * 120;
 		tile = Texture.getTextureTile(Special.CORNER, Orientation.EAST);
-		g.drawImage(tile, xpipes, ypipes, null);
+		g.drawImage(tile, x, y, null);
 
-		xpipes = 0;
-		ypipes += (l.line() + 1) * 120;
+		x = 0;
+		y += (level.line() + 1) * 120;
 		tile = Texture.getTextureTile(Special.CORNER, Orientation.WEST);
-		g.drawImage(tile, xpipes, ypipes, null);
+		g.drawImage(tile, x, y, null);
 
-		xpipes += (l.column() + 1) * 120;
+		x += (level.column() + 1) * 120;
 		tile = Texture.getTextureTile(Special.CORNER, Orientation.SOUTH);
-		g.drawImage(tile, xpipes, ypipes, null);
+		g.drawImage(tile, x, y, null);
 
 		// Dessin des bordures
-		xpipes = 120;
-		ypipes = 0;
+		x = 120;
+		y = 0;
 		tile = Texture.getTextureTile(Special.BORDER);
-		for (int i = 0; i < l.column(); i++) {
-			g.drawImage(tile, xpipes, ypipes, null);
-			xpipes += 120;
+		for (int i = 0; i < level.column(); i++) {
+			g.drawImage(tile, x, y, null);
+			x += 120;
 		}
 
-		xpipes = 120;
-		ypipes = l.line() * 120 + 120;
+		x = 120;
+		y = level.line() * 120 + 120;
 		tile = Texture.getTextureTile(Special.BORDER, Orientation.SOUTH);
-		for (int i = 0; i < l.column(); i++) {
-			g.drawImage(tile, xpipes, ypipes, null);
-			xpipes += 120;
+		for (int i = 0; i < level.column(); i++) {
+			g.drawImage(tile, x, y, null);
+			x += 120;
 		}
 
-		xpipes = 0;
-		ypipes = 120;
+		x = 0;
+		y = 120;
 		tile = Texture.getTextureTile(Special.BORDER, Orientation.WEST);
-		for (int i = 0; i < l.line(); i++) {
-			g.drawImage(tile, xpipes, ypipes, null);
-			ypipes += 120;
+		for (int i = 0; i < level.line(); i++) {
+			g.drawImage(tile, x, y, null);
+			y += 120;
 		}
 
-		xpipes = 120 + (l.column() * 120);
-		ypipes = 120;
+		x = 120 + (level.column() * 120);
+		y = 120;
 		tile = Texture.getTextureTile(Special.BORDER, Orientation.EAST);
-		for (int i = 0; i < l.line(); i++) {
-			g.drawImage(tile, xpipes, ypipes, null);
-			ypipes += 120;
+		for (int i = 0; i < level.line(); i++) {
+			g.drawImage(tile, x, y, null);
+			y += 120;
 		}
 
 		// Dessin interieur puzzle
-		xpipes = 120;
-		ypipes = 120;
+		x = 120;
+		y = 120;
 		tile = Texture.getTextureTile(Special.DARKBROWN);
-		for (int i = 0; i < l.line(); i++) {
-			for (int j = 0; j < l.column(); j++) {
-				g.drawImage(tile, xpipes, ypipes, null);
-				xpipes += 120;
+		for (int i = 0; i < level.line(); i++) {
+			for (int j = 0; j < level.column(); j++) {
+				g.drawImage(tile, x, y, null);
+				x += 120;
 			}
-			xpipes = 120;
-			ypipes += 120;
+			x = 120;
+			y += 120;
 		}
 
+		String[][] state = level.getCurrentState();
+
+		// Dessin sources
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < state.length; j++) {
+				view.Color color = null;
+				view.Orientation orientation = null;
+				String s = state[i * (state.length - 1)][j];
+
+				switch (s.charAt(0)) {
+					case 'X' : continue;
+					case 'R' : color = view.Color.RED; break;
+					case 'G' : color = view.Color.GREEN; break;
+					case 'B' : color = view.Color.BLUE; break;
+					case 'Y' : color = view.Color.YELLOW; break;
+				}
+
+				switch (s.charAt(1)) {
+					case '0' : orientation = Orientation.NORTH; break;
+					case '2' : orientation = Orientation.SOUTH; break;
+				}
+
+				x = j * 120;
+				y = i * (state.length - 1) * 120;
+				tile = Texture.getTextureTile(color, PipeType.SOURCE, orientation);
+				g.drawImage(tile, x, y, null);
+			}
+		}
+
+		for (int i = 0; i < state[0].length; i++) {
+			for (int j = 0; j < 2; j++) {
+				view.Color color = null;
+				view.Orientation orientation = null;
+				String s = state[i][j * (state.length - 1)];
+
+				switch (s.charAt(0)) {
+					case 'X' : continue;
+					case 'R' : color = view.Color.RED; break;
+					case 'G' : color = view.Color.GREEN; break;
+					case 'B' : color = view.Color.BLUE; break;
+					case 'Y' : color = view.Color.YELLOW; break;
+				}
+
+				switch (s.charAt(1)) {
+					case '1' : orientation = Orientation.EAST; break;
+					case '3' : orientation = Orientation.WEST; break;
+				}
+
+				x = j * (state.length - 1) * 120;
+				y = i * 120;
+				tile = Texture.getTextureTile(color, PipeType.SOURCE, orientation);
+				g.drawImage(tile, x, y, null);
+			}
+		}
+
+		// Dessin des visses
+		for (int i = 1; i < state.length - 1; i++) {
+			for (int j = 1; j < state[0].length - 1; j++) {
+				if (state[i][j].charAt(0) == '*') {
+					x = j * 120;
+					y = i * 120;
+					tile = Texture.getTextureTile(Special.DOTS);
+					g.drawImage(tile, x, y, null);
+				}
+			}
+		}
 	}
 
 	@Override
