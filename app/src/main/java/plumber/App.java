a4c4a20c6b5395358b20ce98dbce7a80ab170ea3
Thomas Copt-Bignon <lowx.add@gmail.com>
2021-11-22 19:50:15 +0100

    Workflow test.

--- app/src/main/java/plumber/App.java
+++ app/src/main/java/plumber/App.java
@@ -12,7 +12,6 @@ import javax.swing.*;
 public class App {
     
     public static void main(String[] args) {
-
 		
         Application mainFrame = new Application();
 		mainFrame.setTitle("Plumber");
