dad8c76aeeec58e71cd46cc94223d6a1bb153ef0
Thomas Copt-Bignon <lowx.add@gmail.com>
2021-11-23 19:20:40 +0100

    Removed plumber.App

--- app/src/main/java/application/Application.java
+++ app/src/main/java/application/Application.java
@@ -1,5 +1,9 @@
 package application;
 
+import controller.LevelController;
+import model.Level;
+import view.DrawPanelLevel;
+
 import javax.swing.*;
 
 
@@ -11,5 +15,21 @@ public class Application extends JFrame {
 
 	public void setPanel(JPanel panel) {
 		this.getContentPane().add(panel);
+	}
+
+
+	public static void main(String[] args) {
+
+		Application mainFrame = new Application();
+		mainFrame.setTitle("Plumber");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setResizable(false);
+
+		DrawPanelLevel panelLevel1 = new DrawPanelLevel(new Level(2));
+		mainFrame.setPanel(panelLevel1);
+		LevelController controller1 = new LevelController(panelLevel1);
+
+		mainFrame.pack();
+		mainFrame.setVisible(true);
 	}
 }
