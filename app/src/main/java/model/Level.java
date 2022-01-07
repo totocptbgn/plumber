a5877492ad1350129cde8700d0c0a2b148ecbedc
Thomas <thomasgdb@gmail.com>
2022-01-07 21:00:24 +0100

    Progression with coloring algorithm

--- app/src/main/java/model/Level.java
+++ app/src/main/java/model/Level.java
@@ -66,47 +66,132 @@ public class Level {
 	public void updateColor() {
 		// TODO: remplir colorState
 		this.colorState = new String[this.currentState.length][this.currentState[0].length];
-		ArrayList<Integer[]> colorChecked = new ArrayList<Integer[]>();
-		// On effectue les changements depuis la première et la dernière ligne
+		for(int i = 0; i < colorState.length; i++)
+			for(int j = 0; j < colorState[0].length; j++)
+				colorState[i][j]="";
+		
+		ArrayList<Integer[]> toCheck = new ArrayList<Integer[]>();
+		// On enregistre les points de départs depuis la première et la dernière ligne
 		for(int i = 0; i < currentState.length; i+=currentState.length-1) {
-			for(int j = 0; j < currentState[0].length; j++) {
-				// Est-ce qu'on a déjà vérifié cette source?
-				for(Integer[] alreadyChecked: colorChecked) {
-					if(alreadyChecked[0]==i&&alreadyChecked[1]==j) {
-						continue;
-					}
-				}
-				// Est-ce que cet élément est une source?
-				if(currentState[i][j].contains("R") || currentState[i][j].contains("G")
-						|| currentState[i][j].contains("B") || currentState[i][j].contains("Y")) {
-					String color = currentState[i][j].split("")[0];
-					ArrayList<Integer[]> toCheck = new ArrayList<Integer[]>();
-					Integer[] coordinatesToCheck = {new Integer(i), new Integer(j)};
-					toCheck.add(coordinatesToCheck);
-					// Tant qu'on a un prochain élément connecté à la source à vérifier
-					while(!toCheck.isEmpty()) {
-						for(Integer[] toRemove : toCheck) {
-							toCheck.remove(toRemove);
-							// Est-ce qu'on a déjà vérifié cette tile?
-							boolean hasAlreadyBeenChecked = false;
-							for(Integer[] alreadyChecked: colorChecked) {
-								if(alreadyChecked[0]==toRemove[0]&&alreadyChecked[1]==toRemove[1]) { //TODO: tester si c'est une tile OVER
-									hasAlreadyBeenChecked = true;
-									break;
-								}
-							}
-							if(hasAlreadyBeenChecked) {
-								continue;
-							}
-							colorState[toRemove[0]][toRemove[1]].concat(color);
-							//TODO: choisir les prochaines tiles à check
-						}
-					}
-				}
-				else continue;
-				//this.colorState[0][0].concat("");
+			for(int j = 1; j < currentState[0].length-1; j++) {
+				// Est-ce que la tile hors des cases
+				if(currentState[i][j].equals("X"))
+					continue;
+				else toCheck.add(new Integer[]{i, j});
 			}
 		}
+		// On enregistre les points de départs depuis la première et la dernière colonne
+		for(int i = 1; i < currentState.length-1; i++) {
+			for(int j = 0; j < currentState[0].length; j+=currentState[0].length-1) {
+				// Est-ce que la tile est hors des cases
+				if(currentState[i][j].equals("X"))
+					continue;
+				else toCheck.add(new Integer[]{i, j});
+			}
+		}
+		
+		// On part de chaque source pour colorer les tuyaux
+		for(Integer[] source : toCheck) {
+			String color = "";
+			if(colorState[source[0]][source[1]].equals("")) {
+				if(currentState[source[0]][source[1]].contains("R")) color = "Red";
+				else if(currentState[source[0]][source[1]].contains("G")) color = "Green";
+				else if(currentState[source[0]][source[1]].contains("B")) color = "Blue";
+				else if(currentState[source[0]][source[1]].contains("Y")) color = "Yellow";
+				colorState[source[0]][source[1]]=color;
+				int nextX = -1;
+				int nextY = -1;
+				if(currentState[source[0]][source[1]].contains("0")) {
+					nextX = source[0]-1;
+					nextY = source[1];
+				}
+				else if(currentState[source[0]][source[1]].contains("1")) {
+					nextX = source[0];
+					nextY = source[1]+1;
+				}
+				else if(currentState[source[0]][source[1]].contains("2")) {
+					nextX = source[0]+1;
+					nextY = source[1];
+				}
+				else if(currentState[source[0]][source[1]].contains("3")) {
+					nextX = source[0]-1;
+					nextY = source[1];
+				}
+				if(nextX<1||nextX>currentState.length-1 ||
+						nextY<1||nextY>currentState[0].length-1) {
+					continue;
+					
+				}
+				colorPipe(color, source, new Integer[]{nextX, nextY});
+			}
+			else continue;
+		}
+	}
+	
+	public void colorPipe(String color, Integer[] previous, Integer[] current) {
+		// Est-ce qu'il n'y a pas de tuyaux ?
+		if(currentState[current[0]][current[1]].equals(".")) return;
+		// Cas particulier du tuyau OVER
+		if(currentState[current[0]][current[1]].contains("O")) {
+			
+		}
+		// Est-ce que le tuyau est connecté au précédent ?
+		else if(!arePipesConnected(previous, current)) return;
+		else {
+			// Est-ce que la couleur est déjà établi pour ce tuyau?
+			if(!colorState[current[0]][current[1]].equals("")) return;
+			else {
+				
+			}
+		}
+	}
+
+	private boolean arePipesConnected(Integer[] previous, Integer[] current) {
+		if(currentState[current[0]][current[1]].contains("C"))
+			return true;
+		if(current[0] == previous[0]-1 && current[1] == previous[1]) { // le tuyau précédent est au sud
+			if(currentState[current[0]][current[1]].contains("L")) {
+				if(currentState[current[0]][current[1]].contains("1"))
+					return false;
+				else return true;
+			}
+			if(currentState[current[0]][current[1]].contains("T")) {
+				if(currentState[current[0]][current[1]].contains("0") ||
+						currentState[current[0]][current[1]].contains("3"))
+					return false;
+				else return true;
+			}
+			if(currentState[current[0]][current[1]].contains("F")) {
+				if(currentState[current[0]][current[1]].contains("3"))
+					return false;
+				else return true;
+			}
+		}
+		if(current[0] == previous[0] && current[1] == previous[1]-1) { // le tuyau précédent est à l'est
+			if(currentState[current[0]][current[1]].contains("L")) {
+				if(currentState[current[0]][current[1]].contains("0"))
+					return false;
+				else return true;
+			}
+			if(currentState[current[0]][current[1]].contains("T")) {
+				if(currentState[current[0]][current[1]].contains("2") ||
+						currentState[current[0]][current[1]].contains("3"))
+					return false;
+				else return true;
+			}
+			if(currentState[current[0]][current[1]].contains("F")) {
+				if(currentState[current[0]][current[1]].contains("2"))
+					return false;
+				else return true;
+			}
+		}
+		if(current[0] == previous[0]+1 && current[1] == previous[1]) { // le tuyau précédent est au nord
+			
+		}
+		if(current[0] == previous[0] && current[1]+1 == previous[1]) { // le tuyau précédent est à l'ouest
+			
+		}
+		return false;
 	}
 
 	public boolean isCompleted() {
